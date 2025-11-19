3164f3ddccb14e2f15567d5384bb2cb5791bed33
Zachary <SuperZTaylor@hotmail.com>
2025-11-18 18:04:47 -0600

    Changing port to 10000

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
