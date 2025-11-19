8a60448e7cc6c72bbea62dec9fe8ac3b4b1a5371
Zachary <SuperZTaylor@hotmail.com>
2025-11-18 20:04:57 -0600

    Updated sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+apt-get update && apt-get install -y \
+  libgtk-4-1 libgraphene-1.0-0 libgstgl-1.0-0 \
+  libgstcodecparsers-1.0-0 libenchant-2-2 libsecret-1-0 \
+  libmanette-0.2-0 libgles2
+
 pip install -r requirements.txt
 playwright install
