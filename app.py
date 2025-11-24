2328ed53f404c2733cac5681e66e10ac21d727d5
Zachary <SuperZTaylor@hotmail.com>
2025-11-24 12:15:58 -0600

    Made so custom form takes information

--- app.py
+++ app.py
@@ -10,20 +10,20 @@ def home():
     if request.method == 'POST':
         form_data = request.form.to_dict()
         try:
-            asyncio.run(submit_form())
+            asyncio.run(submit_form(form_data))
             return "Submitted!"
         except Exception as e:
             return f"An error occurred: {e}"
     return render_template('index.html')
 
 
-async def submit_form():
+async def submit_form(form_data):
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True, args=["--no-sandbox"])
         page = await browser.new_page()
         await page.goto("https://fakeformtesting.netlify.app/")
-        await page.fill("input[name='username']", "fliptest")
-        await page.fill("input[name='email']", "fliptest@email.com")
+        await page.fill("input[name='username']", form_data.get('name', ''))
+        await page.fill("input[name='email']", form_data.get('email', ''))
         await page.fill("input[name='password']", "fliptestpass")
         await page.get_by_role("button", name="submit").click()
         await page.wait_for_load_state("networkidle")
